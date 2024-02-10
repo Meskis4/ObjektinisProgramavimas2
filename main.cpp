7bedf6b70ba35b3690ab61850d58dd6fb951497d
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-02-10 16:38:09 +0200

    Vidurkio ir galutinio balo skaičiavimas
    Struktūroje ,,Studentas" pridėjau du kintamuosius vidurkiui ir galutiniam balui saugoti.  Taip pat, dabar programa suskaičiuoja studento vidurkį bei galutinį balą.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ struct Studentas {
     int n;
     int egz;
     int nd[10];
+    double vid;
+    double galut;
 };
 
 const int s_kiek = 3;
@@ -17,12 +19,16 @@ int main()
     Studentas S[s_kiek];
 
     for (int i = 0; i < s_kiek; i++) {
-    cout << "Iveskite savo varda: "; cin >> S[i].vardas;
-    cout << "Iveskite savo pavarde: "; cin >> S[i].pavarde;
-    cout << "Iveskite egzamino bala: "; cin >> S[i].egz;
-    cout << "Iveskite atliktu namu darbu skaiciu: "; cin >> S[i].n;
+        cout << "Iveskite savo varda: "; cin >> S[i].vardas;
+        cout << "Iveskite savo pavarde: "; cin >> S[i].pavarde;
+        cout << "Iveskite egzamino bala: "; cin >> S[i].egz;
+        cout << "Iveskite atliktu namu darbu skaiciu: "; cin >> S[i].n;
+        int paz = 0;
         for (int j = 0; j < S[i].n; j++) {
-        cout << "Iveskite pazymi: "; cin >> S[i].nd[j];
-        }    
+            cout << "Iveskite pazymi: "; cin >> S[i].nd[j];
+            paz += S[i].nd[j];
+        } 
+        S[i].vid = double(paz) / S[i].n;
+        S[i].galut = 0.4 * S[i].vid + 0.6 * S[i].egz;
     }
 }
