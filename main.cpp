3c02b0c321fa9c032dbb21dbf193159f2bab5609
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-02-10 16:26:13 +0200

    Duomenu nuskaitymas
    Pridėjau ciklą, kuris nuskaito studentų duomenis.

--- main.cpp
+++ main.cpp
@@ -15,4 +15,14 @@ const int s_kiek = 3;
 int main()
 {
     Studentas S[s_kiek];
+
+    for (int i = 0; i < s_kiek; i++) {
+    cout << "Iveskite savo varda: "; cin >> S[i].vardas;
+    cout << "Iveskite savo pavarde: "; cin >> S[i].pavarde;
+    cout << "Iveskite egzamino bala: "; cin >> S[i].egz;
+    cout << "Iveskite atliktu namu darbu skaiciu: "; cin >> S[i].n;
+        for (int j = 0; j < S[i].n; j++) {
+        cout << "Iveskite pazymi: "; cin >> S[i].nd[j];
+        }    
+    }
 }
