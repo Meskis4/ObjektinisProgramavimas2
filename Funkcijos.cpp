6f4a61db88544c58b69110ba0c53264528b17d7d
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-02-27 19:39:44 +0200

    Create Funkcijos.cpp
    Sukūriau Funkcijos.cpp failą, kuriame bus aprašytos funkcijos.

---
+++ Funkcijos.cpp
@@ -0,0 +1,16 @@
+#include "Funkcijos.h"
+#include "Studentai.h"
+#include "algorithm"
+
+double Mediana(vector<int>& mas) {
+    sort(mas.begin(), mas.end());
+
+    if (mas.size() % 2 == 0) {
+        double vidur1 = mas[mas.size() / 2 - 1];
+        double vidur2 = mas[mas.size() / 2];
+        return double((vidur1 + vidur2) / 2.0);
+    }
+    else {
+        return double(mas[mas.size() / 2]);
+    }
+}
