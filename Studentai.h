f37f9823355134eef8808ba985654b8cb0b72f42
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-02-27 19:32:25 +0200

    Studentai.h failas
    Sukūriau header failą, skirtą struktūrai apie studentus.

---
+++ Studentai.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Studentas {
+    string vardas;
+    string pavarde;
+    int n;
+    int egz;
+    vector<int> nd;
+    double vid;
+    double galut;
+    double medi;
+    double pasirinkimas;
+};
