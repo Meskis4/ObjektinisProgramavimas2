61a14b10312cbbb3c82850af5635dcb4d7b8a26e
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-03-21 15:52:20 +0200

    Update Funkcijos.h
    Pakeičiau studentų padalijimo į 2 grupes antraštę.

--- Funkcijos.h
+++ Funkcijos.h
@@ -23,7 +23,7 @@ void readFile(const string& fileName, vector<Studentas>& studentai);
 
 void sortStudents(vector<Studentas>& students, int rusiavimas);
 
-void separateStudents(const vector<Studentas>& studentai, vector<Studentas>& Geri, vector<Studentas>& Blogi);
+void generateSeperateFile(const vector<Studentas>& studentai, const string& fileName);
 
 void generateSeperateFile(const vector<Studentas>& studentai, const string& fileName);
 
