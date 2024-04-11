9d8bde14598e0c190a8a686c008aeca925dddf7c
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-04-11 18:54:30 +0300

    Update Studentai.h
    Pakeičiau struktūrą Studentas į klasę.

--- vector/Include/Studentai.h
+++ vector/Include/Studentai.h
@@ -4,21 +4,22 @@
 #include <iostream>
 #include <iomanip>
 #include <random>
+#include <chrono>
 #include <cctype>
 #include <fstream>
 #include <sstream>
 #include <algorithm>
 
 using namespace std;
+class Studentas {
+private:
+    std::string vardas_;
+    std::string pavarde_;
+    int n_;
+    int egz_;
+    std::vector<int> nd_;
+    double vidurkis_;
+    double galutinis_;
+    double mediana_;
 
-struct Studentas {
-    string vardas;
-    string pavarde;
-    int n;
-    int egz;
-    vector<int> nd;
-    double vid;
-    double galut;
-    double medi;
-    double pasirinkimas;
 };
