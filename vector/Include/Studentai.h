253d84e75c0a2359196bca0cc9fa5785a5468055
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-04-12 09:23:58 +0300

    Update Studentai.h
    Pridėjau get'erius

--- vector/Include/Studentai.h
+++ vector/Include/Studentai.h
@@ -21,4 +21,19 @@ private:
     double vidurkis_;
     double galutinis_;
     double mediana_;
+
+
+public:
+    Studentas(); 
+
+//Get'eriai
+    std::string getVardas()const { return vardas_; }
+    std::string getPavarde() const { return pavarde_; }
+    int getN() const { return n_; }
+    int getEgz() const { return egz_; }
+    std::vector<int> getND() const { return nd_; }
+    double getVidurkis() const { return vidurkis_; }
+    double getGalutinis() const { return galutinis_; }
+    double getMediana() const { return mediana_; }
+
 };
