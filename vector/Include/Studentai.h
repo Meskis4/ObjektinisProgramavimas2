768b3265b017338757b1e4130594a085347c5ba0
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-04-12 09:19:32 +0300

    Update Studentai.h

--- vector/Include/Studentai.h
+++ vector/Include/Studentai.h
@@ -4,54 +4,21 @@
 #include <iostream>
 #include <iomanip>
 #include <random>
-#include <chrono>
 #include <cctype>
 #include <fstream>
 #include <sstream>
 #include <algorithm>
 
 using namespace std;
-class Studentas {
-private:
-    std::string vardas_;
-    std::string pavarde_;
-    int n_;
-    int egz_;
-    std::vector<int> nd_;
-    double vidurkis_;
-    double galutinis_;
-    double mediana_;
 
-public:
-    Studentas(); 
-// Get'eriai
-    std::string getVardas()const { return vardas_; }
-    std::string getPavarde() const { return pavarde_; }
-    int getN() const { return n_; }
-    int getEgz() const { return egz_; }
-    std::vector<int> getND() const { return nd_; }
-    double getVidurkis() const { return vidurkis_; }
-    double getGalutinis() const { return galutinis_; }
-    double getMediana() const { return mediana_; }
-
-//Set'eriai
-    void setVardas(const string& vardas) { vardas_ = vardas; }
-    void setPavarde(const string& pavarde) { pavarde_ = pavarde; }
-    void setN(int n) { n_ = n; }
-    void setEgz(int egz) { egz_ = egz; }
-    void setND(const vector<int>& nd) { nd_ = nd; }
-    void setVidurkis(double vidurkis) { vidurkis_ = vidurkis; }
-    void setGalutinis(double galutinis) { galutinis_ = galutinis; }
-    void setMediana(double mediana) { mediana_ = mediana; }
-
-//Metodai
-    int IntInput();
-    void sortStudents(vector<Studentas>& students, int rusiavimas);
-    string allLetters();
-    string StringInput();
-    double Mediana() const;
-    double Vidurkis() const;
-    void ManualDataInput(int pasirinkimas);
-    int generateGrade();
-    void ManualNameInput(int pasirinkimas);
+struct Studentas {
+    string vardas;
+    string pavarde;
+    int n;
+    int egz;
+    vector<int> nd;
+    double vid;
+    double galut;
+    double medi;
+    double pasirinkimas;
 };
