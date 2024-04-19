b5b91770b16859b3b92b16e371f699c8eb0ff77d
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-04-19 22:54:10 +0300

    Update Studentai.h

--- vector/Include/Studentai.h
+++ vector/Include/Studentai.h
@@ -25,13 +25,11 @@ private:
 public:
     Studentas(); 
     ~Studentas();
-    Studentas(const Studentas& other);             //copy konstruktorius
-    Studentas& operator=(const Studentas& s);     // copy priskyrimo operatorius
+    Studentas(const Studentas& other);            //copy konstruktorius
     Studentas(Studentas&& s);                     // move konstruktorius 
+    Studentas& operator=(const Studentas& s);     // copy priskyrimo operatorius
     Studentas& operator=(Studentas&& s);          // move priskyrimo operatorius
 
-
-//Get'eriai
     std::string getVardas()const { return vardas_; }
     std::string getPavarde() const { return pavarde_; }
     int getN() const { return n_; }
@@ -41,7 +39,6 @@ public:
     double getGalutinis() const { return galutinis_; }
     double getMediana() const { return mediana_; }
 
-//Set'eriai
     void setVardas(const string& vardas) { vardas_ = vardas; }
     void setPavarde(const string& pavarde) { pavarde_ = pavarde; }
     void setN(int n) { n_ = n; }
@@ -51,7 +48,6 @@ public:
     void setGalutinis(double galutinis) { galutinis_ = galutinis; }
     void setMediana(double mediana) { mediana_ = mediana; }
 
-//Metodai
 
     int IntInput();
     void sortStudents(vector<Studentas>& students, int rusiavimas);
@@ -59,16 +55,16 @@ public:
     string StringInput();
     double Mediana() const;
     double Vidurkis() const;
-    void ManualDataInput(int pasirinkimas);
     int generateGrade();
-    void ManualNameInput(int pasirinkimas);
     int generateND();
+    string intToString(int value);
+    void generateFile(const std::string& fileName, int studentAmount);
+    void ManualDataInput(int pasirinkimas);
+    void ManualNameInput(int pasirinkimas);
     void GenerateRandomData(const std::vector<std::string>& Vardai_v, const std::vector<std::string>& Vardai_m, const std::vector<std::string>& Pavardes_v, const std::vector<std::string>& Pavardes_m, int pasirinkimas);
     string FailoPatikrinimas();
     void readFile(const string& fileName, vector<Studentas>& studentai, int pasirinkimas);
-    string intToString(int value);
-    void generateFile(const std::string& fileName, int studentAmount);
-    void separateStudents(std::vector<Studentas>& Blogi);
-    void generateSeperateFile(const std::vector<Studentas>& studentai, const std::string& fileName);
+    void separateStudents(vector<Studentas>& studentai, vector<Studentas>& Blogi);
+    void generateSeperateFile(const vector<Studentas>& studentai, const string& fileName, int pasirinkimas);
     void clearVectors(vector<Studentas>& studentai, vector<Studentas>& Blogi);
 };
