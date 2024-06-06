b15c69ffaccb79a76d3af0a23745f2efc3528cbd
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-06-06 16:56:35 +0300

    Update Studentai.h

--- vector/Include/Studentai.h
+++ vector/Include/Studentai.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Zmogus.h"
 #include <vector>
 #include <string>
 #include <iostream>
@@ -9,12 +10,11 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
-#include "Zmogus.h"
-
-using namespace std;
 
 class Studentas : public Zmogus {
 private:
+    std::string vardas_;
+    std::string pavarde_;
     int n_;
     int egz_;
     std::vector<int> nd_;
@@ -23,15 +23,17 @@ private:
     double mediana_;
 
 public:
-    Studentas(); 
+    Studentas();
+    Studentas(const std::string& vardas, const std::string& pavarde, int n, int egz, const std::vector<int>& nd, double vidurkis, double galutinis, double mediana);
     ~Studentas();
-    Studentas(const Studentas& other);            //copy konstruktorius
-    Studentas(Studentas&& s);                     // move konstruktorius 
-    Studentas& operator=(const Studentas& s);     // copy priskyrimo operatorius
-    Studentas& operator=(Studentas&& s);          // move priskyrimo operatorius
 
-    std::string getVardas()const { return vardas_; }
-    std::string getPavarde() const { return pavarde_; }
+    Studentas(const Studentas& other);
+    Studentas& operator=(const Studentas& other);
+    Studentas(Studentas&& other) noexcept;
+    Studentas& operator=(Studentas&& other) noexcept;
+
+    std::string getVardas() const override { return vardas_; }
+    std::string getPavarde() const override { return pavarde_; }
     int getN() const { return n_; }
     int getEgz() const { return egz_; }
     std::vector<int> getND() const { return nd_; }
@@ -39,32 +41,32 @@ public:
     double getGalutinis() const { return galutinis_; }
     double getMediana() const { return mediana_; }
 
-    void setVardas(const string& vardas) { vardas_ = vardas; }
-    void setPavarde(const string& pavarde) { pavarde_ = pavarde; }
+    void setVardas(const std::string& vardas) override { vardas_ = vardas; }
+    void setPavarde(const std::string& pavarde) override { pavarde_ = pavarde; }
     void setN(int n) { n_ = n; }
     void setEgz(int egz) { egz_ = egz; }
-    void setND(const vector<int>& nd) { nd_ = nd; }
+    void setND(const std::vector<int>& nd) { nd_ = nd; }
     void setVidurkis(double vidurkis) { vidurkis_ = vidurkis; }
     void setGalutinis(double galutinis) { galutinis_ = galutinis; }
     void setMediana(double mediana) { mediana_ = mediana; }
 
-
     int IntInput();
-    void sortStudents(vector<Studentas>& students, int rusiavimas);
-    string allLetters();
-    string StringInput();
+    void sortStudents(std::vector<Studentas>& students, int rusiavimas);
+    std::string allLetters();
+    std::string StringInput();
     double Mediana() const;
     double Vidurkis() const;
-    int generateGrade();
-    int generateND();
-    string intToString(int value);
-    void generateFile(const std::string& fileName, int studentAmount);
     void ManualDataInput(int pasirinkimas);
+    int generateGrade();
     void ManualNameInput(int pasirinkimas);
+    int generateND();
     void GenerateRandomData(const std::vector<std::string>& Vardai_v, const std::vector<std::string>& Vardai_m, const std::vector<std::string>& Pavardes_v, const std::vector<std::string>& Pavardes_m, int pasirinkimas);
-    string FailoPatikrinimas();
-    void readFile(const string& fileName, vector<Studentas>& studentai, int pasirinkimas);
-    void separateStudents(vector<Studentas>& studentai, vector<Studentas>& Blogi);
-    void generateSeperateFile(const vector<Studentas>& studentai, const string& fileName, int pasirinkimas);
-    void clearVectors(vector<Studentas>& studentai, vector<Studentas>& Blogi);
+    std::string FailoPatikrinimas();
+    void readFile(const std::string& fileName, std::vector<Studentas>& studentai, int pasirinkimas);
+    std::string intToString(int value);
+    void generateFile(const std::string& fileName, int studentAmount);
+    void separateStudents(std::vector<Studentas>& studentai, std::vector<Studentas>& Blogi);
+    void generateSeperateFile(const std::vector<Studentas>& studentai, const std::string& fileName, int pasirinkimas);
+    void clearVectors(std::vector<Studentas>& studentai, std::vector<Studentas>& Blogi);
+    void printInfo();
 };
