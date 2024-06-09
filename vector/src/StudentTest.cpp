d9fc12a992d1591bed400a047c9b746ecace3581
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-06-09 15:11:34 +0300

    Update StudentTest.cpp
    Pridėtas default konstruktoriaus testas

--- vector/src/StudentTest.cpp
+++ vector/src/StudentTest.cpp
@@ -1,1 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "Studentas.h"
 
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace StudentTest
+{
+	TEST_CLASS(StudentTest)
+	{
+	public:
+		
+		TEST_METHOD(DefaultConstructor)
+		{
+			Studentas A;
+
+			std::string expected = "\t\t0\t0\t\t0.0\t0.0\t0.0";
+
+			Assert::AreEqual(expected, A.output());
+
+		}
+		
+	};
+}
