6feac44fad29af6b395819cff4ccf92ebb7412e2
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-06-09 15:24:54 +0300

    Update StudentTest.cpp
    Pridėtas move konstruktoriaus testas

--- vector/src/StudentTest.cpp
+++ vector/src/StudentTest.cpp
@@ -39,5 +39,17 @@ namespace StudentTest
 
 		}
 
+		TEST_METHOD(MoveConstructor) {
+			Studentas A("Jonas", "Jonaitis", 3, 9, { 1, 5, 4 }, 9.0, 9.4, 9.0);
+			Studentas B(std::move(A));
+
+			std::string expected = "Jonas\tJonaitis\t3\t9\t1 5 4 \t9.0\t9.4\t9.0";
+
+			std::string expected2 = "\t\t0\t0\t\t0.0\t0.0\t0.0";;
+
+			Assert::AreEqual(expected, B.output());
+			Assert::AreEqual(expected2, A.output());
+		}
+
 	};
 }
