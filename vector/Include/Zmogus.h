dca97ea9297cff4c659a598c1403bc196f839291
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-06-09 14:58:25 +0300

    Update Zmogus.h

--- vector/Include/Zmogus.h
+++ vector/Include/Zmogus.h
@@ -1,15 +1,13 @@
 #pragma once
+#include <string>
 
 class Zmogus {
-protected:
-    std::string vardas_;
-    std::string pavarde_;
-
-    Zmogus(const std::string& vardas, const std::string& pavarde)
-        : vardas_(vardas), pavarde_(pavarde) {}
-
 public:
     virtual ~Zmogus() = default;
+
     virtual std::string getVardas() const = 0;
     virtual std::string getPavarde() const = 0;
+
+    virtual void setVardas(const std::string& vardas) = 0;
+    virtual void setPavarde(const std::string& pavarde) = 0;
 };
