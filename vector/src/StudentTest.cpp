3f1eb52d25a20df30bdb1036279e779b4c4a3a3e
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-06-09 15:22:54 +0300

    Update StudentTest.cpp
    Pridėtas copy konstruktoriaus testas

--- vector/src/StudentTest.cpp
+++ vector/src/StudentTest.cpp
@@ -30,5 +30,14 @@ namespace StudentTest
 
 		}
 		
+		TEST_METHOD(CopyConstructor) {
+
+			Studentas A("Jonas", "Jonaitis", 3, 9, { 1, 5, 4 }, 9.0, 9.4, 9.0);
+			Studentas B(A);
+			std::string expected = "Jonas\tJonaitis\t3\t9\t1 5 4 \t9.0\t9.4\t9.0";
+			Assert::AreEqual(expected, B.output());
+
+		}
+
 	};
 }
