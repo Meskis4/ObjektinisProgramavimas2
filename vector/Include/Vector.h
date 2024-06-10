a77f4aa8c59a8c29bdde0b3f9786ef93a401ebc7
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-06-10 16:33:31 +0300

    Update Vector.h

--- vector/Include/Vector.h
+++ vector/Include/Vector.h
@@ -19,6 +19,9 @@ public:
     Vector(Vector&& other) noexcept;
     ~Vector();
 
+  Vector& operator=(const Vector& other);
+  Vector& operator=(Vector&& other) noexcept;
+
 };
 
 // Constructor
@@ -28,3 +31,45 @@ Vector<T>::Vector() : _size(0), _capacity(1), _data(new T[_capacity]) {}
 // Destructor
 template <typename T>
 Vector<T>::~Vector() { delete[] _data; }
+
+// Copy Constructor
+template <typename T>
+Vector<T>::Vector(const Vector& other) : _size(other._size), _capacity(other._capacity), _data(new T[_capacity]) {
+    std::copy(other._data, other._data + other._size, _data);
+}
+
+// Move Constructor
+template <typename T>
+Vector<T>::Vector(Vector&& other) noexcept : _size(other._size), _capacity(other._capacity), _data(other._data) {
+    other._size = 0;
+    other._capacity = 0;
+    other._data = nullptr;
+}
+
+// Copy Assignment Operator
+template <typename T>
+Vector<T>& Vector<T>::operator=(const Vector& other) {
+    if (this != &other) {
+        delete[] _data;
+        _size = other._size;
+        _capacity = other._capacity;
+        _data = new T[_capacity];
+        std::copy(other._data, other._data + other._size, _data);
+    }
+    return *this;
+}
+
+// Move Assignment Operator
+template <typename T>
+Vector<T>& Vector<T>::operator=(Vector&& other) noexcept {
+    if (this != &other) {
+        delete[] _data;
+        _size = other._size;
+        _capacity = other._capacity;
+        _data = other._data;
+        other._size = 0;
+        other._capacity = 0;
+        other._data = nullptr;
+    }
+    return *this;
+}
