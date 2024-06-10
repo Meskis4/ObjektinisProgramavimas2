2855793431d92cea24c5b1cc7bc66a0eb85f78f8
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-06-10 13:13:28 +0300

    Create Vector.h

---
+++ vector/Include/Vector.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <memory>       
+#include <iterator>     
+#include <cstddef>      
+
+template <typename T, typename Allocator = std::allocator<T>>
+class Vector {
+public:
+    // Member types
+    using value_type = T;
+    using allocator_type = Allocator;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = typename std::allocator_traits<Allocator>::pointer;
+    using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
+
+    // Iterator types
+    using iterator = pointer;  
+    using const_iterator = const_pointer; 
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+};
